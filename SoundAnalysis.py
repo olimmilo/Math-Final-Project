2ee31854b735fce4415d207509f99546b70d414c
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-06-07 10:16:23 -0400

    Update SoundAnalysis.py

--- SoundAnalysis.py
+++ SoundAnalysis.py
@@ -4,6 +4,10 @@ import numpy as np #extra math functionality
 from scipy.fftpack import fft #imports a fourier algorithm
 from scipy import signal #signal proscessing functionality
 
+"""
+Initially Defined Functions
+"""
+
 def SoundProscessing(init_sound_list):
 	pass
 
@@ -16,4 +20,38 @@ def QuarticRegression(init_sound_list):
 def Sound_composition(pure_tones,regression)
 	pass
 
-#imports the 
+"""
+PART ONE
+"""
+
+#imports the raw sound and turns it into workable data
+
+#converts the raw sound into a proscessed form which can be used to find the amplitude functions
+
+#finds the linear and quartic amplitude functions
+
+"""
+PART TWO
+"""
+
+#uses the FFT algoritm to proscess the raw sound into its component pure tones and amplitudes therein
+
+#finds the local maxima of the FFT function to determine the discrete component pure tones
+
+"""
+Part Three
+"""
+
+#Creates a list of each individual pure tone
+
+#combines pure tones and amplitude functions into in composite sound wave
+
+"""
+Graphing
+"""
+
+#Graphs the raw soundwave against the two amplitude functions
+
+#Graphs the Fourier Transform of the raw sound wave against a scatter plot of the discretely selected frequencies
+
+#Graphs the composite wave forms using the two different amplitude functions
