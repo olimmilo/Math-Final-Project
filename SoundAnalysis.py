09c3f3350685157424de8035bd1ff45328cb370e
Maisy Wilcox <maisy.wilcox@maisywilcox.com>
2018-06-07 09:39:11 -0400

    Create SoundAnalysis.py

---
+++ SoundAnalysis.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt #imports packages used to allow the code to produce graphs
+import soundfile as sf #imports packages used to allow the code to understand soundfiles
+import numpy as np #extra math functionality
+from scipy.fftpack import fft #imports a fourier algorithm
+from scipy import signal #signal proscessing functionality
+
+def SoundProscessing(init_sound_list):
+	pass
+
+def LinearRegression(init_sound_list):
+	pass
+
+def QuarticRegression(init_sound_list):
+	pass
+	
+def Sound_composition(pure_tones,regression)
+	pass
+
+#imports the 
